4a21190e008e304e680aba93c73b446d541a1c4c
Maks <maksgavrilov956@gmail.com>
2024-07-24 15:15:47 +0300

    second line complete

--- src/pages/Home/components/Content/main/Content.tsx
+++ src/pages/Home/components/Content/main/Content.tsx
@@ -1,10 +1,12 @@
 import FirstLine from "../components/FirstLine/FirstLine";
+import SecondLine from "../components/SecondLine/SecondLine";
 import s from "./Content.module.scss";
 
 const Content = () => {
   return (
     <div className={s.content}>
       <FirstLine />
+      <SecondLine />
     </div>
   );
 };
